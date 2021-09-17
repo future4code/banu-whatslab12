add74021d7874fcd602d4c7075d4d9fc82897b2f
LeonardoSofiati <leonardo.sofiati17@gmail.com>
2021-09-16 23:24:11 -0300

    banu-whatslab12 versao1

---
+++ banu-whatslab12/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+import MessageArea from '../src/components/MessageArea'
+
+class App extends React.Component {
+  render() {
+    return (
+      <MessageArea>
+      </MessageArea>
+    );
+  }
+}
+
+export default App;
---
+++ banu-whatslab12/src/components/InputArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export class InputArea extends React.Component {
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }
+}
+  
